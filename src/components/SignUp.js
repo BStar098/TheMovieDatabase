0e8a43cb1189565b713af74f0e9cd222642cb649
Kamae <luceropintos@abc.gob.ar>
2023-02-06 15:38:59 -0300

    forms validated using react-hook-form module

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,49 +1,74 @@
-import { React, useState } from "react";
+import { React } from "react";
 import "../styles/UserAccess/UserAccess.css";
-import { Button } from "@mui/material";
 import { Person } from "@mui/icons-material";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { signUp } from "../states/users";
+import { useForm } from "react-hook-form";
 
 function SignUp() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [user, setUser] = useState({ email: "", password: "" });
 
-  const signUpHandler = (e) => {
-    console.log(user);
-    setUser((user) => {
-      return { ...user, [e.target.id]: e.target.value };
-    });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  
 
   return (
     <div className="signUpContainer">
-      <form className="signUpInputs">
+      <form
+        onSubmit={handleSubmit((data) => {
+          dispatch(signUp(data)).then(() => {
+            navigate("/me");
+          });
+        })}
+        className="signUpInputs"
+      >
         <Person fontSize="inherit" />
         <input
-          id="email"
-          onChange={signUpHandler}
+          {...register("email", {
+            required: "The email is a required field.",
+            minLength: {
+              value: 8,
+              message: "E-mails must have a minimum length of 8 characters.",
+            },
+            maxLength: {
+              value: 34,
+              message: "E-mails must have a maximum length of 34 characters.",
+            },
+            pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+          })}
+          className="input"
           type="text"
           placeholder="E-mail.."
-        ></input>
+        />
+        {errors.email && (
+          <p className="errorContainer">{errors.email.message}</p>
+        )}
         <input
-          id="password"
-          onChange={signUpHandler}
+          {...register("password", {
+            required: "You must enter a password.",
+            minLength: {
+              value: 8,
+              message: "Passwords must have a minimum length of 8 characters.",
+            },
+            maxLength: {
+              value: 20,
+              message: "Passwords must have a maximum length of 20 characters.",
+            },
+          })}
+          className="input"
           type="password"
           placeholder="Password.."
-        ></input>
-        <Button
-          onClick={() => {
-            dispatch(signUp(user)).then(() => {
-              navigate("/me");
-            });
-          }}
-          className="signUpButton"
-        >
-          Sign Up
-        </Button>
+        />
+        {errors.password && (
+          <p className="errorContainer">{errors.password.message}</p>
+        )}
+        <input className="logInButton" type="submit" value="SIGN UP"></input>
       </form>
     </div>
   );
